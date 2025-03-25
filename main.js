3e31a22f26f5c03084dd0db4216a96a0d65f3c1d
Toshiro Kubota <toshiro732@hotmail.com>
2025-03-24 21:30:35 -0400

    nearly done

--- main.js
+++ main.js
@@ -1,12 +1,45 @@
+const nav = document.getElementById('navbar');
+const menu_button = document.getElementById('menu-button');
 const hamburger = document.getElementById('hamburger-icon');
 const close_icon = document.getElementById('close-icon');
-const menu = document.querySelector('.menu');
-hamburger.addEventListener('click', () => {
-    menu.classList.toggle('hidden');
+
+menu_button.addEventListener('click', () => {
+    nav.classList.toggle('shown');
+    hamburger.classList.toggle('shown');
+    close_icon.classList.toggle('shown');    
+    //console.log(hamburger.classList, close_icon.classList);
 });
-close_icon.addEventListener('click', () => {
-    menu.classList.toggle('hidden');
-});
+// menu_button.addEventListener('keyup', (e) => {
+//     console.log(e.code);
+//     if(e.code == 'Enter') {
+//         nav.classList.toggle('shown');
+//         hamburger.classList.toggle('shown');
+//         close_icon.classList.toggle('shown');   
+//         e.stopPropagation(); 
+//         // if (nav.classList.contains("shown")) {
+//         //   nav.classList.remove("shown");
+//         //   hamburger.classList.add("shown");
+//         //   close_icon.classList.remove("shown");
+//         // } else {
+//         //   nav.classList.add("shown");
+//         //   hamburger.classList.remove("shown");
+//         //   close_icon.classList.add("shown");
+//         // }
+//         console.log(nav);
+//     }
+// });
+// close_icon.addEventListener('click', () => {
+//     nav.classList.toggle('shown');
+//     hamburger.classList.toggle('shown');
+//     close_icon.classList.toggle('shown');
+// });
+// close_icon.addEventListener('keydown', (e) => {
+//     if(e.code == 'Enter') {
+//         nav.classList.toggle('shown');
+//         hamburger.classList.toggle('shown');
+//         close_icon.classList.toggle('shown');
+//     }
+// });
 
 const faq = document.getElementById('faqs');
 const questions = faq.querySelectorAll('.faq-question');
@@ -16,9 +49,14 @@ for(let i=0; i<questions.length; ++i) {
     let q = questions[i];
     let a = answers[i];
     q.addEventListener('click', ()=> {
-        console.log('clicked', q);
         a.classList.toggle('shown');
         q.querySelector('img').classList.toggle('shown');
+    });
+    q.addEventListener('keydown', (e)=> {
+        if(e.code == 'Enter') {
+            a.classList.toggle('shown');
+            q.querySelector('img').classList.toggle('shown');
+        }
     });
 }
 
@@ -31,10 +69,46 @@ for(let i=0; i<items.length; ++i) {
     let item = items[i];
     let f = features[i];
     item.addEventListener('click', () => {
-        console.log('clicked', item, features);
         for(let j=0; j<items.length; ++j) {
             features[j].classList.remove('shown');
+            items[j].classList.remove('shown');
         }
         f.classList.add('shown');
+        item.classList.add('shown');
+    });
+    item.addEventListener('keydown', (e) => {
+        if(e.code == 'Enter') {
+            for(let j=0; j<items.length; ++j) {
+                features[j].classList.remove('shown');
+                items[j].classList.remove('shown');
+            }
+            f.classList.add('shown');
+            item.classList.add('shown');
+        }
     });
 }
+
+const isEmail = (value) => {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(value);
+}
+
+const form = document.querySelector('form');
+const email = form.querySelector('input[type="email"]');
+const error_msg = form.querySelector('.error-msg');
+const error_logo = form.querySelector('.error-logo');
+form.addEventListener('submit', (e)=> {
+    console.log('handling', email.value);
+    if(!isEmail(email.value)) {
+        error_msg.classList.add('shown');
+        error_logo.classList.add('shown');
+        e.preventDefault();
+    } else {
+        email.value='';
+    }
+});
+
+email.addEventListener('focus', () => {
+    error_msg.classList.remove('shown');
+    error_logo.classList.remove('shown');
+});
